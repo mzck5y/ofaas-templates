0e1b761ecf16b699180cf5e142c4bf271ff869c7
Efren Gonzalez <joseefren.gonzalez@gmail.com>
2020-11-05 16:49:59 -0700

    initial commit

---
+++ template/netcore31-func/function/FunctionHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
+namespace function
+{
+    public class FunctionHandler
+    {
+        #region Fields
+
+        private readonly ILogger _logger;
+
+        #endregion
+
+        #region Constructors
+
+        public FunctionHandler(ILogger<FunctionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region Handler
+
+        [HttpPost]
+        public async Task<(int, string)> Handle(HttpRequest request)
+        {
+            using StreamReader reader = new StreamReader(request.Body);
+            string input = await reader.ReadToEndAsync();
+
+            Employee employee = JsonConvert.DeserializeObject<Employee>(input);
+            employee.FirstName = "John";
+            _logger.LogInformation("Function executed....");
+
+            return (201, JsonConvert.SerializeObject(employee));
+        }
+
+        #endregion
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
